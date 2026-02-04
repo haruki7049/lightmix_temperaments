4841d41825fbada86ca41946c9af92c9df36e924
haruki7049 <tontonkirikiri@gmail.com>
2026-02-04 13:01:20 +0900

    Update root.zig

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,7 @@
 //! lightmix_temperaments
 
 pub const TwelveEqualTemperament = @import("./twelve_equal_temperament.zig");
+
+test "Import each modules' tests" {
+    _ = @import("./twelve_equal_temperament.zig");
+}
